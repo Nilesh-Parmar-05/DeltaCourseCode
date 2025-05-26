f4ddc15c40686446806095e99193172321b156b7
Nilesh <nileshparmar2005@gmail.com>
2025-05-27 00:05:10 +0530

    Add route to serve fruit list as HTML response

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -6,3 +6,10 @@ let port = 3000;
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
+
+app.use((req, res) => {
+  console.log("request recieved");
+  let code =
+    "<h1>Fruits</h1> <ul><li>Apple</li><li>Banana</li><li>Cherry</li></ul>";
+  res.send(code);
+});
